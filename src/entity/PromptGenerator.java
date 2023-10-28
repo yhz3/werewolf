fa860511c4da94370a8f6271f27b7cfead15ad66
Yunfei (Kevin) Wang <123699163+yunfeikevinwang@users.noreply.github.com>
2023-10-28 13:25:09 -0400

    Revised to include group suggestions

--- src/entity/PromptGenerator.java
+++ src/entity/PromptGenerator.java
@@ -29,26 +29,19 @@ public class PromptGenerator {
     public String generatePlayerKilledPrompt(String playerKilled) {
         String preamble = "One night has passed and this player has been killed: ";
         String instruction = ". Describe in detail how they were killed. Reveal the victim's name only at the end of " +
-                "the story. Hidden in the story, include a extremely subtle and ambiguous hint of who the murderer " +
+                "the story. Hidden in the story, include a misleading hint of who the murderer " +
                 "might be. Do not point out the hint explicitly, it should be buried deep inside like a mystery story.";
         String prompt = conversationHistory.getConversationHistory() + preamble + playerKilled + instruction;
         conversationHistory.addUserMessage(prompt);
         return prompt;
     }
 
-    public String generatePlayerVotedOutPrompt(String playerVotedOut, boolean isWerewolf) {
+    public String generatePlayerVotedOutPrompt(String playerVotedOut, String playerRole) {
         String preamble = "The villagers have decided to vote out: ";
         String instruction = ". Continue the story, describing the player's last words, and reveal their true identity" +
                 " at the end. Remember that they are a ";
 
-        String identity;
-        if (isWerewolf) {
-            identity = "Werewolf";
-        } else {
-            identity = "Villager";
-        }
-
-        String prompt = conversationHistory.getConversationHistory() + preamble + playerVotedOut + instruction + identity;
+        String prompt = conversationHistory.getConversationHistory() + preamble + playerVotedOut + instruction + playerRole;
         conversationHistory.addUserMessage(prompt);
         return prompt;
     }
