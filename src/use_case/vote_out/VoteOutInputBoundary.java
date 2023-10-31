92499592e15c5f9b4f6d7f993096a705db538137
daniyaalf14 <dquantum14@gmail.com>
2023-10-31 14:02:41 -0400

    working on interactor and input data of vote out use case

--- src/use_case/vote_out/VoteOutInputBoundary.java
+++ src/use_case/vote_out/VoteOutInputBoundary.java
@@ -1,4 +1,5 @@
 package use_case.vote_out;
 
 public interface VoteOutInputBoundary {
+    void voteOut(VoteOutInputData voteOutInputData);
 }
--- src/use_case/vote_out/VoteOutInteractor.java
+++ src/use_case/vote_out/VoteOutInteractor.java
@@ -1,4 +1,17 @@
 package use_case.vote_out;
 
 public class VoteOutInteractor implements VoteOutInputBoundary {
+    final VoteOutDataAccessInterface userDataAccessObject;
+    final VoteOutOutputBoundary userPresenter;
+
+    public VoteOutInteractor(VoteOutDataAccessInterface userDataAccessObject, VoteOutOutputBoundary userPresenter) {
+        this.userDataAccessObject = userDataAccessObject;
+        this.userPresenter = userPresenter;
+    }
+
+
+    @Override
+    public void voteOut(VoteOutInputData voteOutInputData) {
+
+    }
 }
