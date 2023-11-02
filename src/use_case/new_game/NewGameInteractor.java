98af0db5e02ec670f259cd6314de5275e3d73519
emily <emily.langenfeld@mail.utoronto.ca>
2023-11-02 10:29:47 -0400

    Started Interactor implementation

--- src/use_case/new_game/NewGameInteractor.java
+++ src/use_case/new_game/NewGameInteractor.java
@@ -1,4 +1,16 @@
 package use_case.new_game;
+import entity.Game;
+import entity.Player;
+import entity.Werewolf;
+import java.util.ArrayList;
 
 public class NewGameInteractor {
+
+    public void assignRole(Player player){
+
+    }
+
+    public void execute(ArrayList<String> PlayerNames){
+        int numWerewolves = PlayerNames.size() / 3;
+    }
 }
