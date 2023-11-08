6f2a7608fe2cfc81d863490f7463d5c70d11ef91
daniyaalf14 <dquantum14@gmail.com>
2023-11-08 09:31:17 -0500

    i renamed the data access object to GameDataAccessObject which is a slightly cleaner name. I also made it implement my vote out data access interface. Everyone needs to have this class implement the data access interface they create and implement its methods. Refer to cacoding for an example since that's how they do it there.

---
+++ src/data_access/GameDataAccessObject.java
@@ -0,0 +1,12 @@
+package data_access;
+
+import entity.Game;
+import use_case.vote_out.VoteOutDataAccessInterface;
+
+public class GameDataAccessObject implements VoteOutDataAccessInterface {
+    // TODO: this class must implement all data access interfaces we create going forward
+    @Override
+    public void save(Game game) {
+
+    }
+}
