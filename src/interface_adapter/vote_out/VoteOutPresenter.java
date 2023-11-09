b3051e8f5c5db1ab7053c9ea292055afbb32eeab
daniyaalf14 <dquantum14@gmail.com>
2023-11-09 10:41:06 -0500

    Changed my presenter to reflect the changes I made with the output boundary (now the success view takes in the output data as a parameter

--- src/interface_adapter/vote_out/VoteOutPresenter.java
+++ src/interface_adapter/vote_out/VoteOutPresenter.java
@@ -1,16 +1,18 @@
 package interface_adapter.vote_out;
 
 import use_case.vote_out.VoteOutOutputBoundary;
+import use_case.vote_out.VoteOutOutputData;
 
 public class VoteOutPresenter implements VoteOutOutputBoundary {
     // TODO: do it
+
     @Override
-    public void prepareFailView() {
+    public void prepareFailView(String error) {
 
     }
 
     @Override
-    public void prepareSuccessView() {
+    public void prepareSuccessView(VoteOutOutputData player) {
 
     }
 }
