679cd867389aff72bf5a3e4d45dc2069faeba876
yhz3 <105172127+yhz3@users.noreply.github.com>
2023-11-10 17:32:20 -0500

    updated kill villager data interface and interactor

--- src/use_case/kill_villager/KillVillagerInteractor.java
+++ src/use_case/kill_villager/KillVillagerInteractor.java
@@ -2,11 +2,14 @@ package use_case.kill_villager;
 
 import entity.Game;
 import entity.PromptGenerator;
-import use_case.ChatAPIAccessInterface;
+import use_case.data_access_interface.ChatAPIAccessInterface;
+import use_case.data_access_interface.ConversationDataAccessInterface;
+import use_case.data_access_interface.GameDataAccessInterface;
 
 public class KillVillagerInteractor implements KillVillagerInputBoundary{
 
-    private final KillVillagerDataAccessInterface killVillagerDataAccessInterface;
+    private final ConversationDataAccessInterface conversationDataAccessObject;
+    private final GameDataAccessInterface gameDataAccessObject;
     private final KillVillagerOutputBoundary killVillagerPresenter;
     private final PromptGenerator promptGenerator;
     private final ChatAPIAccessInterface gptDataAccessObject;
@@ -14,17 +17,19 @@ public class KillVillagerInteractor implements KillVillagerInputBoundary{
     // uniformity.
     private final Game game;
 
-    public KillVillagerInteractor(KillVillagerDataAccessInterface killVillagerDataAccessInterface,
+    public KillVillagerInteractor(ConversationDataAccessInterface conversationDataAccessObject,
+                                  GameDataAccessInterface gameDataAccessObject,
                                   KillVillagerOutputBoundary killVillagerPresenter,
                                   ChatAPIAccessInterface gptDataAccessObject) {
-        this.killVillagerDataAccessInterface = killVillagerDataAccessInterface;
+        this.conversationDataAccessObject = conversationDataAccessObject;
+        this.gameDataAccessObject = gameDataAccessObject;
         this.killVillagerPresenter = killVillagerPresenter;
         // TODO: Figure out how to talk to the Output Boundary without violating CA, i.e. where the output boundary is
         //  being passed in from.
         // Notice how we are getting the prompt generator and game from the killVillagerDataAccessInterface,
         // adhering to the CA Engine.
-        this.promptGenerator = killVillagerDataAccessInterface.getPromptGenerator();
-        this.game = killVillagerDataAccessInterface.getGame();
+        this.promptGenerator = conversationDataAccessObject.getPromptGenerator();
+        this.game = gameDataAccessObject.getGame();
         this.gptDataAccessObject = gptDataAccessObject;
         // TODO: Figure out how to talk to ChatGPT without violating CA, since passing in gptDAO as a parameter means it
         //  is coming in as an input from the controller, which is not supposed to be able to talk to the DAO.
@@ -42,13 +47,14 @@ public class KillVillagerInteractor implements KillVillagerInputBoundary{
             // Get the story
             String villagerDeathPrompt = promptGenerator.generatePlayerKilledPrompt(villager);
             String villagerDeathStory = gptDataAccessObject.getResponse(villagerDeathPrompt);
+
             // Kill villager
             game.killPlayer(villager);
             // Here I am assuming that the game entity will properly update the aliveVillagers hashmap and properly
             // update the deadVillagers hashmap.
             // Switch to day
             game.changeGameState();
-            killVillagerDataAccessInterface.save(game);
+            gameDataAccessObject.save(game);
             KillVillagerOutputData killVillagerOutputData = new KillVillagerOutputData(villagerDeathStory);
             killVillagerPresenter.prepareSuccessView(killVillagerOutputData);
             // If we follow Daniyaal's implementation, then we would also pass the name along as output data in case
