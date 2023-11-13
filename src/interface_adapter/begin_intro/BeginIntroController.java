ee354ff48a8549992715a0ef1e6c3ffb9536aaab
Yunfei (Kevin) Wang <123699163+yunfeikevinwang@users.noreply.github.com>
2023-11-13 16:38:37 -0500

    Finished begin_intro interface_adapter

--- src/interface_adapter/begin_intro/BeginIntroController.java
+++ src/interface_adapter/begin_intro/BeginIntroController.java
@@ -1,2 +1,15 @@
-package interface_adapter.begin_intro;public class BeginIntroController {
+package interface_adapter.begin_intro;
+
+import use_case.begin_intro.BeginIntroInputBoundary;
+import use_case.begin_intro.BeginIntroInputData;
+
+public class BeginIntroController {
+     final BeginIntroInputBoundary beginIntroInteractor;
+
+    public BeginIntroController(BeginIntroInputBoundary beginIntroInteractor) {
+        this.beginIntroInteractor = beginIntroInteractor;
+    }
+    public void execute(BeginIntroInputData beginIntroInputData) {
+        this.beginIntroInteractor.execute(beginIntroInputData);
+    }
 }
--- src/interface_adapter/begin_intro/BeginIntroPresenter.java
+++ src/interface_adapter/begin_intro/BeginIntroPresenter.java
@@ -1,2 +1,23 @@
-package interface_adapter.begin_intro;public class BeginIntroPresenter {
+package interface_adapter.begin_intro;
+
+import interface_adapter.ViewManagerModel;
+import use_case.begin_intro.BeginIntroOutputBoundary;
+import use_case.begin_intro.BeginIntroOutputData;
+
+import javax.swing.text.View;
+
+public class BeginIntroPresenter implements BeginIntroOutputBoundary {
+    private final BeginIntroViewModel beginIntroViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public BeginIntroPresenter(BeginIntroViewModel beginIntroViewModel, ViewManagerModel viewManagerModel) {
+        this.beginIntroViewModel = beginIntroViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void prepareSuccessView(BeginIntroOutputData beginIntroOutputData) {
+        beginIntroViewModel.setIntroStory(beginIntroOutputData.getIntroStory());
+        beginIntroViewModel.firePropertyChanged();
+    }
 }
