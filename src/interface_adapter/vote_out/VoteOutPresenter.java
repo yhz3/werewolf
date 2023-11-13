24404277681a5e02c673a494e6a3ec3653dbb669
daniyaalf14 <dquantum14@gmail.com>
2023-11-13 16:42:37 -0500

    Started VoteOutPresenter, need view manager stuff before I can continue. Not using states to store data and instead will store it inside the view model. Success view updates view model's parameters.

--- src/interface_adapter/vote_out/VoteOutPresenter.java
+++ src/interface_adapter/vote_out/VoteOutPresenter.java
@@ -4,15 +4,28 @@ import use_case.vote_out.VoteOutOutputBoundary;
 import use_case.vote_out.VoteOutOutputData;
 
 public class VoteOutPresenter implements VoteOutOutputBoundary {
-    // TODO: do it
 
-    @Override
-    public void prepareFailView(String error) {
+    private final VoteOutViewModel voteOutViewModel;
 
+    public VoteOutPresenter(VoteOutViewModel voteOutViewModel) {
+        this.voteOutViewModel = voteOutViewModel;
     }
 
     @Override
     public void prepareSuccessView(VoteOutOutputData player) {
-
+        String playerVotedOut = player.getPlayerVotedOut();
+        String playerRole = player.getPlayerRole();
+        String story = player.getStory();
+        this.voteOutViewModel.setPlayerVotedOut(playerVotedOut);
+        this.voteOutViewModel.setPlayerRole(playerRole);
+        this.voteOutViewModel.setStory(player.getStory());
+        // fire property changed
     }
+
+    @Override
+    public void prepareFailView(String error) {
+        this.voteOutViewModel.setError(error);
+        // fire property changed
+    }
+
 }
