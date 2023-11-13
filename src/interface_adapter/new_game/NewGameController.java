773722e9d7194ddfab016e28166ea4268f491903
emily <emily.langenfeld@mail.utoronto.ca>
2023-11-13 16:33:17 -0500

    Resolving merge conflict

--- src/interface_adapter/new_game/NewGameController.java
+++ src/interface_adapter/new_game/NewGameController.java
@@ -1,4 +1,20 @@
 package interface_adapter.new_game;
 
+import use_case.new_game.NewGameInputBoundary;
+import use_case.new_game.NewGameInputData;
+
+import java.util.ArrayList;
+
 public class NewGameController {
+
+    private final NewGameInputBoundary newGameInteractor;
+
+    public NewGameController(NewGameInputBoundary newGameInteractor){
+        this.newGameInteractor = newGameInteractor;
+    }
+
+    public void execute(ArrayList<String> PlayerNames){
+        NewGameInputData newGameInputData = new NewGameInputData(PlayerNames);
+        newGameInteractor.execute(newGameInputData);
+    }
 }
--- src/interface_adapter/new_game/NewGamePresenter.java
+++ src/interface_adapter/new_game/NewGamePresenter.java
@@ -1,9 +1,19 @@
 package interface_adapter.new_game;
 
+import use_case.new_game.NewGameInputBoundary;
 import use_case.new_game.NewGameOutputBoundary;
 import use_case.new_game.NewGameOutputData;
 
 public class NewGamePresenter implements NewGameOutputBoundary {
+
+    private final NewGameViewModel newGameViewModel;
+
+    private final ViewManagerModel viewManagerModel;
+
+    public NewGamePresenter(NewGameViewModel newGameViewModel, ViewManagerModel viewManagerModel){
+        this.newGameViewModel = newGameViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
 
     public void prepareSuccessView(NewGameOutputData newGameOutputData){}
 }
