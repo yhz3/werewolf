275f11160fa3a79c26cc7f9b49e7169c1a6bb519
daniyaalf14 <dquantum14@gmail.com>
2023-11-14 16:21:16 -0500

    Since we're modelling our views based on the view model / view model manager system from week5ca, my VoteOutViewModel now extends ViewModel (abstract class) and my presenter uses firePropertyChanged(). Instead of using states I'm setting up my code right now to store attributes in the view model

--- src/interface_adapter/vote_out/VoteOutPresenter.java
+++ src/interface_adapter/vote_out/VoteOutPresenter.java
@@ -1,11 +1,14 @@
 package interface_adapter.vote_out;
 
+import interface_adapter.ViewManagerModel;
 import use_case.vote_out.VoteOutOutputBoundary;
 import use_case.vote_out.VoteOutOutputData;
 
 public class VoteOutPresenter implements VoteOutOutputBoundary {
 
     private final VoteOutViewModel voteOutViewModel;
+    private ViewManagerModel viewManagerModel;
+
 
     public VoteOutPresenter(VoteOutViewModel voteOutViewModel) {
         this.voteOutViewModel = voteOutViewModel;
@@ -20,12 +23,13 @@ public class VoteOutPresenter implements VoteOutOutputBoundary {
         this.voteOutViewModel.setPlayerRole(playerRole);
         this.voteOutViewModel.setStory(player.getStory());
         // fire property changed
+        this.voteOutViewModel.firePropertyChanged();
     }
 
     @Override
     public void prepareFailView(String error) {
         this.voteOutViewModel.setError(error);
-        // fire property changed
+        this.voteOutViewModel.firePropertyChanged();
     }
 
 }
--- src/interface_adapter/vote_out/VoteOutViewModel.java
+++ src/interface_adapter/vote_out/VoteOutViewModel.java
@@ -1,10 +1,18 @@
 package interface_adapter.vote_out;
 
-public class VoteOutViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+
+public class VoteOutViewModel extends ViewModel {
     private String playerVotedOut;
     private String playerRole;
     private String story;
     private String error;
+
+    public VoteOutViewModel(String viewName) {
+        super(viewName);
+    }
 
     public void setPlayerVotedOut(String playerVotedOut) {
         this.playerVotedOut = playerVotedOut;
@@ -20,5 +28,15 @@ public class VoteOutViewModel {
 
     public void setError(String error) {
         this.error = error;
+    }
+
+    @Override
+    public void firePropertyChanged() {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+
     }
 }
