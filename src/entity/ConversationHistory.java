2ff3e6d1739be47d64179676d7fe159cfab2b636
Yunfei (Kevin) Wang <123699163+yunfeikevinwang@users.noreply.github.com>
2023-11-24 17:23:07 -0500

    Fixed comments in ConversationHistory

--- src/entity/ConversationHistory.java
+++ src/entity/ConversationHistory.java
@@ -2,25 +2,6 @@ package entity;
 
 import java.util.ArrayList;
 
-/*
-Idea for compression algorithm
-- Use ChatGPT-3.5 for compression
-1. Prompt: Summarize the following so that it can be used on ChatGPT for context.
-2. Prompt: Summarize the following conversation so that it can be used on ChatGPT for context.
-
-1. If message is more than 1 turn old, compress using ChatGPT-3.5
-    - Thus, prompts to ChatGPT will only have the most recent reply that is uncompressed
-    - getConversationHistory right before generating a prompt, so most recent message should be AI
-    - Following prompt1, reply1 scheme, it should be that the last prompt1
-      (and the invisible reply0 before) messages are compressed
-    - Alternative is to compress last 2 after saving what we will return
-2. If length of history is 6 or more, compress oldest 3 into 1. So 6 -> 4
-    - Thus, it will be: prompt1, reply1, prompt2, reply2, prompt3, fresh_reply3
-    - Right before sending prompt4, it becomes summary1, reply2, prompt3, fresh_reply3
-    - And then fresh_reply3 is compressed to reply3 by
-    - This should be performed before returning conversation history
- */
-
 public class ConversationHistory {
     private final ArrayList<String> history;
 
@@ -48,12 +29,12 @@ public class ConversationHistory {
     public String getConversationToCompress() {
         int historyLength = history.size();
 
-        // If the history length is more than 3, we will remove the messages until there remains only one message and
-        // return the history
+        // If the history length is more than 3 add them to messageToCompress, and then remove them
         if (historyLength > 3) {
             StringBuilder messageToCompress = new StringBuilder();
             for (int i = 0; i < historyLength - 1; i++) {
                 messageToCompress.append(history.get(i));
+                messageToCompress.append("\n\n");
             }
             history.subList(0, 3).clear();
             return messageToCompress.toString();
