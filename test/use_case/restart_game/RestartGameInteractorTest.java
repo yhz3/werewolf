76fee285bf827457bfc177ea60b6510e8e589c98
yhz3 <105172127+yhz3@users.noreply.github.com>
2023-12-02 12:34:48 -0500

    updated test cases for higher coverage

---
+++ test/use_case/restart_game/RestartGameInteractorTest.java
@@ -0,0 +1,12 @@
+package use_case.restart_game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestartGameInteractorTest {
+
+    @Test
+    void execute() {
+    }
+}
