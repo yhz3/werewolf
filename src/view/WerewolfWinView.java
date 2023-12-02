b4abd0660ab6fb536569134a1ceb04b9cc2772fb
Yunfei (Kevin) Wang <123699163+yunfeikevinwang@users.noreply.github.com>
2023-12-02 15:15:38 -0500

    Added the Story button back to the win views

--- src/view/WerewolfWinView.java
+++ src/view/WerewolfWinView.java
@@ -1,11 +1,11 @@
 package view;
 
 import interface_adapter.check_win.CheckWinController;
-import interface_adapter.check_win.VillagerWinViewModel;
 import interface_adapter.check_win.WerewolfWinState;
 import interface_adapter.check_win.WerewolfWinViewModel;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
@@ -19,6 +19,8 @@ public class WerewolfWinView extends JPanel implements ActionListener, PropertyC
 
     private final CheckWinController checkWinController;
 
+    private final JButton viewStoryButton;
+
     private final JButton newGameButton;
 
     public WerewolfWinView(WerewolfWinViewModel werewolfWinViewModel, CheckWinController checkWinController){
@@ -28,11 +30,36 @@ public class WerewolfWinView extends JPanel implements ActionListener, PropertyC
         this.werewolfWinViewModel.addPropertyChangeListener(this);
 
         JLabel title = new JLabel(WerewolfWinViewModel.TITLE_LABEL);
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         JPanel buttons = new JPanel();
 
+        viewStoryButton = new JButton(WerewolfWinViewModel.STORY_BUTTON_LABEL);
+        buttons.add(viewStoryButton);
+
         newGameButton = new JButton(WerewolfWinViewModel.NEW_GAME_BUTTON_LABEL);
         buttons.add(newGameButton);
+
+        viewStoryButton.addActionListener(
+                new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent evt){
+                        if (evt.getSource().equals(viewStoryButton)){
+                            WerewolfWinState state = werewolfWinViewModel.getState();
+                            String story = state.getWerewolfWinStory().replace("\\n", "\n");
+                            JTextArea textArea = new JTextArea(15, 30);
+                            textArea.setText(story);
+                            textArea.setWrapStyleWord(true);
+                            textArea.setLineWrap(true);
+                            textArea.setCaretPosition(0);
+                            textArea.setEditable(false);
+
+                            JScrollPane scrollPane = new JScrollPane(textArea);
+                            JOptionPane.showMessageDialog(textArea, scrollPane);
+                        }
+                    }
+                }
+        );
 
         newGameButton.addActionListener(
                 new ActionListener() {
